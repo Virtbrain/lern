2feb252b708c0a373da482965f6ab94ecd7a0e76
Alexey <virtbrain@gmail.com>
2017-12-29 22:58:45 +0500

    Listing2_4

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,26 +1,18 @@
-function f(x){
-    if(x<0){
-        return 0
-    }
-    else{
-        if(x<2){
-            return x*x
-        }
-        else{
-            if(x<5){
-                return 6-x
-            }
-            else{
-                return 1
-            }
-        }
-    }
+//Верхняя граница суммы
+var n=100;
+//Индексная переменная
+var k=1;
+//Начальное значение суммы квадратов
+var s=0;
+//текст для отображения
+var txt="1<sup>2</sup> + 2<sup>2</sup>+ ... +";
+txt+=n+"<sup>2</sup>= ";
+
+//Вычислдение суммы квадратов чисел:
+while(k<=n){
+    s+=k*k; //Добавление нового слагаемого
+    k++; //Новое значение индексной переменной
 }
 
-document.write("<h4>Кусочно-гладкая функция</h4>")
-document.write('<img src="function.jpg" wight="500" height="300" alt="нет картинки"><br>')
-
-var z
-z = 9*Math.random()-2
-document.write("Случайный аргумент: <b>"+z+"</b><br>")
-document.write("Значение функции: <b>"+f(z)+"</b>")
+//Отображение результата
+document.write(txt+s)
