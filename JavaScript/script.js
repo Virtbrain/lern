afd8e771b48e595b12812a716ea0bc030a1abbf7
Alexey <virtbrain@gmail.com>
2017-12-29 15:04:16 +0500

    Listing2_3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,26 @@
-var txt, name, msg
-txt = "Давайте познакомимся! Как Вас зовут?"
-msg = "Жаль, но Вы не представились!"
-
-name = prompt(txt)
-if(name!=""){
-    msg = "Приятно вознакомиться, "+name+"!"
+function f(x){
+    if(x<0){
+        return 0
+    }
+    else{
+        if(x<2){
+            return x*x
+        }
+        else{
+            if(x<5){
+                return 6-x
+            }
+            else{
+                return 1
+            }
+        }
+    }
 }
-document.write(msg)
+
+document.write("<h4>Кусочно-гладкая функция</h4>")
+document.write('<img src="function.jpg" wight="500" height="300" alt="нет картинки"><br>')
+
+var z
+z = 9*Math.random()-2
+document.write("Случайный аргумент: <b>"+z+"</b><br>")
+document.write("Значение функции: <b>"+f(z)+"</b>")
