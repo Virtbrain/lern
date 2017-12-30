319c70e20f0895b6476f7046d8585cf69e19b803
Alexey <virtbrain@gmail.com>
2017-12-30 14:37:57 +0500

    Listing2_6

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,9 @@
-var n=100, k=1, s=0;
+var n=100, s=0, k;
 var txt="1<sup>2</sup>+2<sup>2</sup>+ ... + ";
 txt += n+"<sup>2</sup> = ";
-do{
+
+for(k=1;k<=n;k++){
     s+=k*k;
-    k++;
-}while(k<=n);
+}
 
 document.write(txt+s)
