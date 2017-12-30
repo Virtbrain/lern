f1fdca80d02899b3cd7c564d5516720632925d0c
Alexey <virtbrain@gmail.com>
2017-12-30 15:01:22 +0500

    Listion2_10

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,5 +2,11 @@ var n=100, s=0, k=1;
 var txt="1<sup>2</sup>+2<sup>2</sup>+ ... + ";
 txt += n+"<sup>2</sup> = ";
 
-for(;k<=n;s+=k*k,k++);
+for(;;){
+    s+=k*k;
+    k++;
+    if(k>n){
+        break;
+    }
+}
 document.write(txt+s);
