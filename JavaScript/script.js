9b604397f2b67b5d6d2b506b763e4bffb923f7cc
Alexey <virtbrain@gmail.com>
2017-12-31 13:48:23 +0500

    Listing3_6

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,10 @@
 var myText = "Глобальная переменная"
 document.write(myText + "<br>")
 function show(){
-    myText = "Переменная с намеком на глобальность"
-    document.write(myText+"<br>")
-    
     var myText = "Локальная переменная"
-    document.write(myText + "<br>")
+    window.myText = "Переменная с намеком на глобальность"
+    document.write(myText+"<br>")
+    document.write(window.myText + "<br>")
 }
 
 show()
