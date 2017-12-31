389aefa76868616e9219a25ba8f21de1e0ff2a40
Alexey <virtbrain@gmail.com>
2017-12-31 12:45:38 +0500

    Listing3_2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,10 @@
-makeHeader("Знакомимся с функциями")
+var myText = "Глобальная переменная"
+document.write(myText+"<br>")
 
-function getRandText(n){
-    var txt="Случайные числа (от 1 до 10):<br>* "
-    for(var k=1;k<=n;k++){
-        txt+=myRand()+" * "
-    }
-    txt+="<br>"
-    return txt
+function show(){
+    var myText = "Локальная переменная"
+    document.write(myText+"<br>")
 }
 
-document.write(getRandText(20))
-
-function myRand(){
-    return 1+Math.floor(10*Math.random())
-}
-
-function makeHeader(t){
-    document.write("<h4>"+t+"</h4>")
-}
+show()
+document.write(myText+"<br>")
