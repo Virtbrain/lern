51985420ddcc8bd013acf3d67a4b941bdb36b576
Alexey <virtbrain@gmail.com>
2018-01-05 17:35:03 +0500

    Listing3_10

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,19 @@
-function f(x){
-    document.write("Аргумент: "+x+"<br>")
-    x++
-    document.write("Аргумент: "+x+"<br>")
+function power(x,n){
+    if(typeof(n)=="undefined"){
+        n=1
+    }
+    if(typeof(x)=="undefined"){
+        x=1
+    }
     
+    var s=1,k
+    
+    for(k=1;k<=n;k++){
+        s*=x
+    }
+    return s
 }
 
-var num=100
-document.write("переменная: "+num+"<br>")
-f(num)
-document.write("переменная: "+num+"<br>")
+document.write("2<sup>3</sup>= "+power(2,3)+"<br>")
+document.write("5<sup>1</sup>= "+power(5)+"<br>")
+document.write("1<sup>1</sup>= "+power()+"<br>")
