9f88a0a6287b466641affdf59ac58c188984dec5
Alexey <virtbrain@gmail.com>
2018-01-07 15:05:17 +0500

    Listing3_15

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,11 @@
-function mlog(x){
-    var s=0,k
-    var n=100
-    for(k=1;k<=n;k++){
-        s-=power(-x,k)/k
-    }
-    return s
-    function power(z,m){
-        var p=1,i
-        for(i=1;i<=m;i++){
-            p*=z
-        }
-        return p
-    }   
-}
+show ("Функция show()")
+var f
+f = show
+f("Теперь это функцийя f()")
+show = "Текстовое значение"
+document.write(show+"<br>")
 
-document.write("<h4>использование внутренней функции</h4>")
-var x=0.5
-document.write("ln(1+"+x+")= "+mlog(x)+"<br>")
-document.write("Проверка: "+Math.log(1+x)+"<br>")
+f("Снова функция f()")
+function show(msg){
+    document.write(msg+"<br>")
+}
