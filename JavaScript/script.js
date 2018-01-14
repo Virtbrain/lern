ca092e33b14e02ad09c5966578ddac13d242b5aa
Alexey <virtbrain@gmail.com>
2018-01-14 14:53:36 +0500

    Listing4_15

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,16 +1,35 @@
-var A={}
-var B=Object.create(null)
-var C=Object.create(B)
-B.number=100
-Object.prototype.name = "Объект А"
-
-show(A)
-show(B)
-show(C)
-
-function show(obj){
-    for(var s in obj){
-        document.write(s+" -> "+obj[s])    
+function MyObject(name,number){
+    this.name = name
+    this.number = number
+    this.show=function(){
+        for(var s in this){
+            if(s!="show"){
+                document.write(s+" -> "+this[s]+" | ")
+            }
+        }
+        document.write("<br>")
     }
-    document.write("<br>")
 }
+
+var A = new MyObject("Объект А",100)
+var B = new MyObject("Объект В",200)
+
+document.write("<b>Свойства созданных объектов:</b><br>")
+A.show()
+B.show()
+
+MyObject.prototype.color="Прозрачный"
+document.write("<b>После добавления свойства:</b><br>")
+A.show()
+B.show()
+
+A.color = "Белый"
+
+delete MyObject.prototype.color
+document.write("<b>После удаления свойства:</b><br>")
+A.show()
+B.show()
+
+document.write("<b>Проверка прототипа объекта</b><br>")
+var txt="Object.getPrototypeOf(A)==MyObject.prototype"
+document.write(txt+" -> "+eval(txt))
