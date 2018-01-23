6cea2b7d198baf08ce807472dc3ea5334a1d2f75
Alexey <virtbrain@gmail.com>
2018-01-23 20:57:12 +0500

    Listing5_9

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,40 +1,12 @@
-with(document){
-    var A=[5,3,2]
-    write("A = ["+A+"]<br>")
-    write("Результат выполенния команды <code>A.reduce(Math.pow)</code>:<br>")
-    write(A.reduce(Math.pow)+"<br>")
-    
-    write("Результат выполенния команды <code>A.reduceRight(Math.pow)</code>:<br>")
-    write(A.reduceRight(Math.pow)+"<br>")
-    
-    var B=[1,2,,4,5,6,7,,9]
-    write("B =["+B+"]<br>")
-    write("результат выполнения команды <code>B.forEach(fillIt)</code>:<br>")
-    B.forEach(fillIt)
-    write("B =["+B+"]<br>")
-    B.splice(2,1)
-    write("После выполнения команды <code>B.splice(2,1)</code>:<br>")
-    write("B =["+B+"]<br>")
-    B.splice(6,1)
-    write("После выполнения команды <code>B.splice(6,1)</code>:<br>")
-    write("B =["+B+"]<br>")
-    write("После выполнения команды <code>B.some(isIt)</code>:<br>")
-    write(B.some(isIt)+"<br>")
-    write("Результат выполенния команды <code>B.every(isInRange)</code>:<br>")
-    write(B.every(isInRange)+"<br>")
-    write("Результат выполнения команды <code>A.every(isInRange)</code>:<br>")
-    write(A.every(isInRange)+"<br>")
-}
+var A=[1,2,3,4,5]
+document.write("A = ["+A+"]<br>")
+var B=["a","b","c"]
+document.write("B = ["+B+"]<br>")
 
-function fillIt(value, index, array){
-    array[index]*=10
-    document.write(index+": "+value+"<br>")
-}
+B=A
+document.write("После присваивания <code>B = A</code>:<br>")
+document.write("B = ["+B+"]<br>")
 
-function isIt(value, index, array){
-    return (index<=array.length/2)&&(value<30)
-}
-
-function isInRange(value, index, array){
-    return (value>0)&&(value<50)
-}
+A[0]=100
+document.write("После присваивания <code>A[0] = 100</code>:<br>")
+document.write("B = ["+B+"]<br>")
