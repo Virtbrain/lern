02085082cf075bdb69bffc3d6ace0c825d574fb2
Alexey <virtbrain@gmail.com>
2018-01-27 23:37:01 +0500

    Listing6_3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,29 +1,25 @@
-try{
-    document.write("Начало выполнения <code>try</code>-блока<br>")
-    var txt=prompt("Введите выражение для вычисления:")
-    document.write("Результат выражения: "+eval(txt)+"<br>")
-    document.write("Выполнение <code>try</code>-блока завершено<br>")
-    
-}
-catch(e){
-    var name=e.name
-    var message=e.message
-    var str
-    switch(name){
-        case "ReferenceError":
-            str="Некоректная ссылка"
-            break
-        case "SyntaxError":
-            str="Синтаксическая ошибка"
-            break
-        default:    
-        sr="Ошибка "+name
+var A=[2,5,9,1,0,3,7,8,6,4]
+var num
+for(var k=0;k<A.length;k++){
+    num=A[k]
+    try{
+        if(num==0){
+            throw "нуелевое значение"
+        }
+        document.write("Отличное от нуля число: "+num+"<br>")
     }
-    document.write("<b>Внимание! Произошла ошибка!</b><br>")
-    document.write("<b>Тип ошибки: </b>"+str+"<br>")
-    
-    if ("fileName" in e){
-        document.write("<b>Файл с ошибкой: </b>"+e.fileName+"<br>")
+    catch(e){
+        document.write("Внимание: "+e+"<hr>")
+        continue
     }
+    
+    try{
+        if(num%3==0){
+            throw new Error("Число денлится на три")
+        }
+    }
+    catch(e){
+        document.write(e.message)
+    }
+    document.write("<hr>")
 }
-document.write("Выполнение сценария завершено")
