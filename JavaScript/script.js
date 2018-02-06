f9ada4cbf01c1be6ade97b04a468f0cb1a69e4e2
Alexey <virtbrain@gmail.com>
2018-02-06 23:21:33 +0500

    Listing6_8

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,57 +1,27 @@
-Object.prototype.toString=function(){
-    var t="<u>Собственные свойства объекта:</u><br>"
-    for(var s in this){
-        t+="<b>"+s+"</b>: "+this[s]+"<br>"
-    }
-    return t
-}
-
-Object.prototype.valueOf=function(){
-    return Object.keys(this).length
-}
-
-var A={one:100,two:200,three:300,four:400,five:500}
-document.write(A)
-document.write("Количество свойств в объекте - "+A+"<br>")
-
-var B={first:"первый",second:"второй",therd:"третий"}
-document.write(B)
-document.write("Количество свойств в объекте - "+B+"<br>")
-
-function Person(name, age, gender){
-    var g
-    if(gender){
-        g="муж."
-    }
-    else{
-        g="жен."
-    }
-    this.name=name
-    this.age=age
-    Object.defineProperty(this,"gender",{
-        get:function(){
-            return g
+function Tree(rank,refs,number,code){
+    this.rank=rank
+    this.number=number || 1
+    this.code=code || "| "
+    this.code+=this.rank+" -> "+this.number+" | "
+    if(this.rank>1){
+        this.next=new Array(refs)
+        for(var k=0;k<this.next.length;k++){
+            this.next[k]=new Tree(this.rank-1,refs,k+1,this.code)
         }
-    })
+    }
+    
 }
 
-Person.prototype.toString=function(){
-    var t="<u>Персональные данные:</u><br>"
-    t+="<b>Имя:</b>" +this.name+"<br>"
-    t+="<b>Возраст:</b>"+this.age+"<br>"
-    t+="<b>Пол:</b>"+this.gender+"<br>" 
+Tree.prototype.toString=function(){
+    var t="Объект: ранг - "+this.rank+",номер -"+this.number
+    t+=",код - "+this.code+"<br>"
+    if(this.rank>1){
+        for(var k=0;k<this.next.length;k++){
+            t+=this.next[k].toString()
+        }
+    }
     return t
 }
 
-Person.prototype.valueOf=function(){
-    return this.age
-}
-
-var X=new Person("Ирина",20,false)
-var Y=new Person("Андрей",25,true)
-
-document.write(X)
-document.write(Y)
-
-document.write("Возраст Ирины - "+X+" лет<br>")
-document.write("Возраст Андрея - "+Y+" лет<br>")
+var myTree=new Tree(4,2)
+document.write(myTree)
