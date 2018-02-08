8409547f3b11f2f9c659f1f28a49f3d40b3e0170
Alexey <virtbrain@gmail.com>
2018-02-09 00:28:16 +0500

    Listing6_10

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,45 +1,20 @@
-function testArgs(func){
-    var n=func.length
-    var words
-    switch(n){
-        case 0:
-            words="Нет аргументов"
-            break
-        case 1:
-            words="Один аргумент"
-            break
-        case 2:
-        case 3:
-        case 4:
-            words=n+" аргумента"
-            break
-        default:
-            words=n+" аргументов."
-    }
-    document.write("Функция "+func+": "+words+"<br>")
-}
-
-function show(){
-    document.write("Всем привет")
-}
-function F(a,b,c,d,e,f){
-    return a*b*c*d*e*f
-}
-
-testArgs(eval)
-testArgs(Math.pow)
-testArgs(show)
-testArgs(F)
-
-function getFunc(f1,f2){
-    if(f1.length<=f2.length){
-        return f1
+function sum(){
+    var s
+    if((arguments.length>0)&&(typeof(arguments[0])=="string")){
+        s=""
     }
     else{
-        return f2
+        s=0
     }
+    for(var k=0;k<arguments.length;k++){
+        s+=arguments[k]
+    }
+    return s
 }
 
-var x=getFunc(Math.pow,F)(2,3)
-document.write(x+"<br>")
-getFunc(show,eval)()
+d = document
+d.write("Сумма чисел: "+sum(1,4,2,7,3)+"<br>")
+d.write("Сумма чисел: "+sum(1,2,3)+"<br>")
+d.write("Сумма чисел: "+sum()+"<br>")
+d.write("Сумма чисел: "+sum("один","четыре","два","семь","три")+"<br>")
+
