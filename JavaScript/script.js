0ee563da3cb4b8b7adcc352da38da0b5de49cfe0
Alexey <virtbrain@gmail.com>
2018-02-12 20:20:00 +0500

    Listing6_11

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,35 @@
-function sum(){
-    var s
-    if((arguments.length>0)&&(typeof(arguments[0])=="string")){
-        s=""
-    }
-    else{
-        s=0
-    }
-    for(var k=0;k<arguments.length;k++){
-        s+=arguments[k]
-    }
-    return s
+
+function f(text,number){
+    this.text=text
+    this.number=number
 }
 
-d = document
-d.write("Сумма чисел: "+sum(1,4,2,7,3)+"<br>")
-d.write("Сумма чисел: "+sum(1,2,3)+"<br>")
-d.write("Сумма чисел: "+sum()+"<br>")
-d.write("Сумма чисел: "+sum("один","четыре","два","семь","три")+"<br>")
+var A={}
+var B={show:function(){
+    for(var s in this){
+        document.write(s+": "+this[s]+"<br>")
+    }
+}}
 
+var C={}
+C.method=f
+f("функция",100)
+f.call(A,"Объект А",200)
+f.apply(B,["Объект B",300])
+C.method("Объект C",400)
+
+document.write(text+" | "+number+"<br>")
+document.write(A.text+" | "+A.number+"<br>")
+document.write(B.text+" | "+B.number+"<br>")
+document.write(C.text+" | "+C.number+"<br>")
+
+document.write(("f" in A)+"<br>")
+document.write(("f" in B)+"<br>")
+document.write(("f" in C)+"<br>")
+
+document.write("<b>Объект B:</b><br>")
+B.show()
+document.write("<b>Объект A:</b><br>")
+B.show.call(A)
+document.write("<b>Объект C:</b><br>")
+B["show"].call(C)
