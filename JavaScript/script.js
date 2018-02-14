224c2aea50d7c40df07c450157d358efef519eeb
Alexey <virtbrain@gmail.com>
2018-02-14 23:49:28 +0500

    Listing6_16

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,15 @@
-var myTrue=new Boolean(true)
-var myFalse=new Boolean(false)
-if(myTrue){
-    document.write("Объект myTrue<br>")
+var text="Мы изучаем JavaScript"
+document.write("<b>"+text+"</b><br>")
+for(var k=11;k<text.length;k++){
+    document.write(text[k])
 }
 
-if(myTrue==true){
-    document.write("Снова объект myTrue<br>")
-}
-
-if(myFalse){
-    document.write("Объект myFalse<br>")
-}
-
-if(myFalse==false){
-    document.write("Снова объект myFalse")
-}
+document.write("<br>"+text.slice(0,10))
+document.write("<br>"+text.slice(11))
+document.write("<br>"+text.substring(11))
+document.write("<br>"+text.substr(11))
+document.write("<br>"+text.toUpperCase())
+document.write("<br>"+text.toLowerCase())
+document.write("<br>"+text.search("Java"))
+document.write("<br>"+text.replace("изучаем","любим"))
+document.write("<br><b>"+text+"</b>")
